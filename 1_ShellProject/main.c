1bf0728315e07593a59dc890d44cc161d4822900
MattHorton <mh409@evansville.edu>
2019-05-07 23:51:02 -0500

    cleaned up code in main

--- 1_ShellProject/main.c
+++ 1_ShellProject/main.c
@@ -5,10 +5,8 @@
 #define MAX_LINE 80 /* The maximum length command */
 #define LSH_TOK_DELIM " \t\r\n\a"
 
-char* getCommand();
-
-
-
+void getCommand();
+void parseCommand();
 
 int numArgs = 0;
 char** args;
@@ -17,24 +15,20 @@ char* line;
 
 
 int main(int argc, char** argv) {
-
     //char *args[MAX_LINE/2 + 1]; /* command line arguments */
-
     int running = 1; /* flag to determine when to exit program */
-
-    
-
 
     while (running) {
         printf("Number of arguments: %d\n", numArgs);
         for(int x = 0; x < numArgs; x++) {
             printf("\n%s\n", args[x]);
         }
-        /**
+        /*
         * After reading user input, the steps are:
         * (1) fork a child process using fork()
         * (2) the child process will invoke execvp()
-        * (3) if command included &, parent will invoke wait() */
+        * (3) if command included &, parent will invoke wait()
+        */
        //fflush(stdout);
        getCommand();
        system("clear");
@@ -43,7 +37,8 @@ int main(int argc, char** argv) {
 }
 
 
-char* getCommand() {
+
+void getCommand() {
     size_t len = 0;
     //char* line = NULL;
     int position = 0;
